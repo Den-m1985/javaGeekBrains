f0d6a927b51c2804ff5ad456eca4bb3bbb60e573
den-m1985 <denismartyanov244@gmail.com>
2023-01-23 20:43:38 +0200

    seminar1 add teacher code

--- Seminar01/Program.java
+++ Seminar01/Program.java
@@ -5,18 +5,25 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         // https://vertex-academy.com/tutorials/ru/java-8-new-date-time-api-chast-1/
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(now);
-        //Scanner iScaner = new Scanner(System.in);
-        //System.out.print("Enter something: ");
-        //String name = iScaner.next();
+        //LocalDateTime now = LocalDateTime.now();
+        //System.out.println(now);
+        // прописываем кодировку для кирилицы.
+        String encoding = System.getProperty("console.encoding","cp866");
+        Scanner iScaner = new Scanner(System.in, encoding);
+        System.out.print("Enter something: ");
+        String name = iScaner.next();
         //System.out.println(name);
-        //iScaner.close();
-        Print(now);
+        String temp = Print(name);
+        System.out.println(temp);
+        iScaner.close();
+
     }
-    public static void Print(String name){
+    public static String Print(String name){
         LocalTime now = LocalTime.now();
-        if (now.isAfter(LocalTime.of(5,0)))
-            System.out.println(now);
+        if (now.isAfter(LocalTime.of(5,0)) && now.isBefore(LocalTime.of(11,59)))
+            return "Good morning " + now;
+        else if (now.isAfter(LocalTime.of(12,0)) && now.isBefore(LocalTime.of(19,59)))
+            return "Good day " + now;
+        return "Good evening " + now;
     }
 }
