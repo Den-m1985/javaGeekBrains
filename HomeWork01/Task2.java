677c6be9ed47febf9d0bef650f4c821719e4c9e6
den-m1985 <denismartyanov244@gmail.com>
2023-01-24 12:11:32 +0200

    first work task2

--- HomeWork01/Task2.java
+++ HomeWork01/Task2.java
@@ -1,6 +1,10 @@
 public class Task2 {
     //Вывести все простые числа от 1 до 1000
     public static void main(String[] args) {
-
+        int a = 20; // подставляем нужное число
+        System.out.println("Простые числа от 1 до " + a);
+        for (int i = 1; i < a+1; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
