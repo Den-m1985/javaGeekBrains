a95a371cc1429e1677531675c1c59eb583e872ff
den-m1985 <denismartyanov244@gmail.com>
2023-01-25 20:29:24 +0200

    redid task 2

--- HomeWork01/Task2.java
+++ HomeWork01/Task2.java
@@ -1,10 +1,35 @@
 public class Task2 {
     //Вывести все простые числа от 1 до 1000
     public static void main(String[] args) {
-        int a = 20; // подставляем нужное число
-        System.out.println("Простые числа от 1 до " + a);
-        for (int i = 1; i < a+1; i++) {
-            System.out.print(i + " ");
+
+        int count = 1;
+        int n = 100;//число до которого необходимо найти все простые числа
+        for (int i = 1; i <= n; i++) {
+            if (simpleNumbers(i)) {
+                count++;//счетчик простых чисел
+                System.out.print(i + " ");
+            }
         }
+        System.out.println();
+        System.out.println("Количество простых чисел: " + count);//вывод количества
     }
+
+    static boolean simpleNumbers(int i) {//берем число i
+
+        if (i<=1)
+            return false;
+        else if (i <=3)
+            return true;
+        else if (i%2==0 || i %3 ==0)
+            return false;
+        int n = 5;
+        while (n*n <=i){
+            if (i % n ==0 || i % (n+2) == 0)
+                return false;
+            n=n+6;
+        }
+        return true;
+    }
+
 }
+
