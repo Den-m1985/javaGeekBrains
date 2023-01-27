cbec44f9517b5a05e6ecead48d8ce873547035e4
den-m1985 <denismartyanov244@gmail.com>
2023-01-27 14:00:49 +0200

    add task home work 2

--- HomeWork02/Task1.java
+++ HomeWork02/Task1.java
@@ -1,5 +1,12 @@
 public class Task1 {
-    //
+    // Дана строка sql-запроса "select * from students where ".
+    // Сформируйте часть WHERE этого запроса, используя StringBuilder.
+    // Данные для фильтрации приведены ниже в виде json строки.
+    //Если значение null, то параметр не должен попадать в запрос.
+    //Параметры для фильтрации:
+    // String str = "{'name':'Ivanov', 'country':'Russia', 'city':'Moscow', 'age':'null'}";
+    //Результат "select * from students where “name” = “Ivanov” and “country”=”Russia” …
+
     public static void main(String[] args) {
 
     }
---
+++ HomeWork02/Task4.java
@@ -0,0 +1,7 @@
+public class Task4 {
+    // К калькулятору из предыдущего дз добавить логирование.
+
+    public static void main(String[] args) {
+
+    }
+}
