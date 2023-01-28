44bce8797775005cb8e39a73bc87391703458188
den-m1985 <denismartyanov244@gmail.com>
2023-01-28 18:22:36 +0200

    add code from home work 1

--- HomeWork02/Task4.java
+++ HomeWork02/Task4.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
+
 public class Task4 {
     // К калькулятору из предыдущего дз добавить логирование.
 
     public static void main(String[] args) {
+        try {
+            Scanner scaner = new Scanner(System.in);
+            while (true) {
+                System.out.println("\n Для выхода введите q");
 
+                System.out.print("Введите первое число: ");
+                int a = scaner.nextInt();
+
+                System.out.print("Введите математическую операцию: ");
+                String x = scaner.next();
+
+                System.out.print("Введите второе число: ");
+                int b = scaner.nextInt();
+
+                if (x.equals("q")) break;
+                else if (x.equals("+")) Sum(a, b);
+                else if (x.equals("-")) Minus(a, b);
+                else if (x.equals("*")) Mul(a, b);
+                else if (x.equals("/")) Divide(a, b);
+            }
+            scaner.close();
+        }catch (Exception e){
+            System.out.println("Ошибка");
+        }
     }
+
+
+    public static void Sum(int a,int b){
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+    public static void Minus(int a,int b){
+        System.out.println(a + " - " + b + " = " + (a - b));
+    }
+    public static void Mul(int a,int b){
+        System.out.println(a + " * " + b + " = " + (a * b));
+    }
+    public static void Divide(double a,double b){
+        System.out.println(a + " / " + b + " = " + (a / b));
+    }
+
 }
