47b40c98df64a91991ecbd53b557bd1bce519ac4
den-m1985 <denismartyanov244@gmail.com>
2023-02-02 11:25:31 +0200

    added Task

--- HomeWork04/Task01.java
+++ HomeWork04/Task01.java
@@ -1,2 +1,9 @@
 public class Task01 {
+    /*
+    Пусть дан LinkedList с несколькими элементами.
+    Реализуйте метод, который вернет “перевернутый” список.
+     */
+    public static void main(String[] args) {
+
+    }
 }
