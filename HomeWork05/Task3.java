385a4721b60a72a81a42213f3ed8db79cde220df
den-m1985 <denismartyanov244@gmail.com>
2023-02-07 16:55:25 +0200

    this code need delete

--- HomeWork05/Task3.java
+++ HomeWork05/Task3.java
@@ -1,8 +1,27 @@
+import java.util.*;
+
 public class Task3 {
     /*
      Реализовать алгоритм пирамидальной сортировки (HeapSort).
      */
     public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(5, "five");
+        map.put(11, "eleven");
+        map.put(4, "four");
+        map.put(77, "seventy seven");
+        map.put(9, "nine");
+
+        System.out.println("HashMap before sorting: "+map);
+
+        //Sorting HashMap by keys using TreeMap
+        Map<Integer, String> sorted = new TreeMap<>(map);
+
+        System.out.println("HashMap after sorting by keys: "+sorted);
+
+
+
 
     }
 }
