2c897d15ef858249729aaf88b73f49911f07dea7
den-m1985 <denismartyanov244@gmail.com>
2023-02-07 16:54:47 +0200

    intermediate variant

--- HomeWork05/Task2.java
+++ HomeWork05/Task2.java
@@ -1,8 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Task2 {
     /*
@@ -19,34 +17,52 @@ public class Task2 {
 
 
     // Читаем из файла
-    public static Map<String, String> readFile() throws FileNotFoundException {
+    public static void readFile() throws FileNotFoundException {
 
+        Map<String, Integer> contact = new HashMap<>();
+        List<String> words = new ArrayList<>();
 
-        TreeMap<String, String> words = new TreeMap<>();
+        // Читаем файл, берем только имя.
         Scanner filescanner = new Scanner(new FileReader("Task2.txt"));
         while (filescanner.hasNextLine()) {
             String[] columns = filescanner.nextLine().split(": ");
-            //words.put(columns[0], columns[1]);
-
-        double p = 0.1;
-        for (String string : columns) {
-            //words.put(string, string);
-            System.out.println(string);
+            words.add(columns[0]);
         }
-//                if (words.containsKey((double) string.length())) {  // Если containsKey содержит такую длину
-//                    words.put((double) string.length() + p, string);  // то кладем с увеличиным на р
-//                    // если мы в один ключ закиним несколько значение, то они перетрутся и поряются слова.
-//                    p += 0.1;
-//                } else
-//                    words.put((double) string.length(), string);
 
+
+
+
+        for (String s : words) {
+            if (contact.containsKey(s)) {  // кладем значение и получаем ключ, если он есть
+                contact.put(s, contact.get(s) + 1);
+            } else {
+                contact.put(s, 1);
+            }
         }
-//            for (var el : words.entrySet()) {
-//                System.out.print(el.getKey() + " " + el.getValue() + "\n");
-//            }
 
 
-        return words;
+        //contact.entrySet().forEach(System.out::println);
+        // Сортируем по ключу
+        Map<String, Integer > sorted = new TreeMap<>(contact);
+        sorted.entrySet().forEach(System.out::println);
+
+        //printHashMap(contact);
+
+    }
+
+    public static void FindSameName(){
+
+    }
+
+    public static void sortByKey(){
+
+    }
+
+    // Печатаем HashMap
+    public static void printHashMap(Map<String, Integer> names) {
+        for (Map.Entry<String, Integer> el : names.entrySet()) {
+            System.out.print(el.getKey() + ": " + el.getValue() + "\n");
+        }
     }
 
 
