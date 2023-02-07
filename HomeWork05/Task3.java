bd83b708a2a017785f93bb625c85caac621794a0
den-m1985 <denismartyanov244@gmail.com>
2023-02-07 20:01:44 +0200

    work version Task3

--- HomeWork05/Task3.java
+++ HomeWork05/Task3.java
@@ -5,23 +5,77 @@ public class Task3 {
      Реализовать алгоритм пирамидальной сортировки (HeapSort).
      */
     public static void main(String[] args) {
+        int[] array = fillArray();
 
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(5, "five");
-        map.put(11, "eleven");
-        map.put(4, "four");
-        map.put(77, "seventy seven");
-        map.put(9, "nine");
+        System.out.print("Origin array: ");
+        printArray(array);
 
-        System.out.println("HashMap before sorting: "+map);
+        sort(array);
 
-        //Sorting HashMap by keys using TreeMap
-        Map<Integer, String> sorted = new TreeMap<>(map);
+        System.out.print("Sorted array: ");
+        printArray(array);
 
-        System.out.println("HashMap after sorting by keys: "+sorted);
+    }
+
+    public static int[] fillArray() {
+        int length = 10;
+        int [] array = new int[length];
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int rand = random.nextInt(10);
+            array[i] = rand;
+        }
+        return array;
+    }
+
+    public static void sort(int [] arr) {
+        int n = arr.length;
+
+        // Строим кучу.
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // Извлекаем элементы из кучи.
+        for (int i = n - 1; i >= 0; i--) {
+            // Перемещаем текущую в конец
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            // Вызываем процедуру heapify на уменьшенной куче
+            heapify(arr, i, 0);
+        }
+    }
 
 
+    // Для кучи поддерева, укорененного в узле i, который является индексом в arr[], n- размер кучи
+    static void heapify(int [] arr, int n, int i) {
+
+        int largest = i;
+        int l = 2 * i + 1; // Левый
+        int r = 2 * i + 2; // Правый
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(arr, n, largest);
+        }
+
+    }
 
 
+    public static void printArray(int [] arr)
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
     }
 }
