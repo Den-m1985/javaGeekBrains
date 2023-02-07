f902f6c34db5b5f6df38e292fc47b0d127fc241a
den-m1985 <denismartyanov244@gmail.com>
2023-02-07 20:02:25 +0200

    added methods Task2

--- HomeWork05/Task2.java
+++ HomeWork05/Task2.java
@@ -11,27 +11,48 @@ public class Task2 {
      Отсортировать по убыванию популярности.
      */
     public static void main(String[] args) throws FileNotFoundException {
-        readFile();
+        List<String> words = readFile();
+        Map<String, Integer> contact = findSameName(words);
+        Map<String, Integer > sorted = sortByKey(contact);
+        printHashMap(sorted);
 
+        /*
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(5, "five");
+        map.put(11, "eleven");
+        map.put(4, "four");
+        map.put(77, "seventy seven");
+        map.put(9, "nine");
+
+        System.out.println("HashMap before sorting: " + map);
+
+        //Sorting HashMap by keys using TreeMap
+        Map<Integer, String> sorted = new TreeMap<>(map);
+
+        System.out.println("HashMap after sorting by keys: " + sorted);
+
+         */
     }
 
 
-    // Читаем из файла
-    public static void readFile() throws FileNotFoundException {
+    // Читаем из файла Task2.txt
+    public static List<String> readFile() throws FileNotFoundException {
 
-        Map<String, Integer> contact = new HashMap<>();
         List<String> words = new ArrayList<>();
-
         // Читаем файл, берем только имя.
         Scanner filescanner = new Scanner(new FileReader("Task2.txt"));
         while (filescanner.hasNextLine()) {
             String[] columns = filescanner.nextLine().split(": ");
             words.add(columns[0]);
         }
+        return words;
+    }
 
 
+    // Находим одинаковые имена
+    public static Map<String, Integer> findSameName(List<String> words){
 
-
+        Map<String, Integer> contact = new HashMap<>();
         for (String s : words) {
             if (contact.containsKey(s)) {  // кладем значение и получаем ключ, если он есть
                 contact.put(s, contact.get(s) + 1);
@@ -39,31 +60,22 @@ public class Task2 {
                 contact.put(s, 1);
             }
         }
+        return contact;
+    }
 
 
-        //contact.entrySet().forEach(System.out::println);
-        // Сортируем по ключу
+    public static Map<String, Integer > sortByKey(Map<String, Integer> contact){
         Map<String, Integer > sorted = new TreeMap<>(contact);
-        sorted.entrySet().forEach(System.out::println);
-
-        //printHashMap(contact);
-
+        //sorted.entrySet().forEach(System.out::println);
+        return sorted;
     }
 
-    public static void FindSameName(){
-
-    }
-
-    public static void sortByKey(){
-
-    }
 
     // Печатаем HashMap
     public static void printHashMap(Map<String, Integer> names) {
         for (Map.Entry<String, Integer> el : names.entrySet()) {
-            System.out.print(el.getKey() + ": " + el.getValue() + "\n");
+            System.out.print(el.getKey() + "= " + el.getValue() + "\n");
         }
     }
-
 
 }
