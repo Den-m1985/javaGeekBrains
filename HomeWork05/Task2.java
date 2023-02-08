40180db356094e58ec9acf9ef76f2b6d36c59332
den-m1985 <denismartyanov244@gmail.com>
2023-02-08 11:11:44 +0200

    work version

--- HomeWork05/Task2.java
+++ HomeWork05/Task2.java
@@ -16,22 +16,7 @@ public class Task2 {
         Map<String, Integer > sorted = sortByKey(contact);
         printHashMap(sorted);
 
-        /*
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(5, "five");
-        map.put(11, "eleven");
-        map.put(4, "four");
-        map.put(77, "seventy seven");
-        map.put(9, "nine");
 
-        System.out.println("HashMap before sorting: " + map);
-
-        //Sorting HashMap by keys using TreeMap
-        Map<Integer, String> sorted = new TreeMap<>(map);
-
-        System.out.println("HashMap after sorting by keys: " + sorted);
-
-         */
     }
 
 
