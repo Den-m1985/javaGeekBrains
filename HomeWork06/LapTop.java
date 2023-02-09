c8cc0b61a96b8c17d5d39552839d5418f551cb17
den-m1985 <denismartyanov244@gmail.com>
2023-02-09 16:05:51 +0200

    first commit

--- HomeWork06/LapTop.java
+++ HomeWork06/LapTop.java
@@ -1,3 +1,18 @@
 public class LapTop {
 
+    private String nameCompany;
+    private String memoryRAM;
+    private String volumeHD;
+    private String operatingSystem;
+    private String color;
+
+    public LapTop(String name, String memoryRAM, String volumeHD, String operatingSystem, String color) {
+        this.nameCompany = name;
+        this.memoryRAM = memoryRAM;
+        this.volumeHD = volumeHD;
+        this.operatingSystem = operatingSystem;
+        this.color = color;
+    }
+
+
 }
