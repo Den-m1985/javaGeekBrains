ebebe441639e819b638ead74e7536dff8b229fd0
den-m1985 <denismartyanov244@gmail.com>
2023-02-13 10:08:05 +0200

    added code and check it

--- HomeWork06/Task2.java
+++ HomeWork06/Task2.java
@@ -3,29 +3,33 @@ import java.util.*;
 public class Task2 {
 
     public static void main(String[] args) {
+
         Task1.NewLaptop(Task1.lapTopBase);
-        sortLapTop();
-
+        SortNameAlphabetically(Task1.shop);
 
     }
 
-    private static void sortLapTop() {
 
-        Map<String, String> unsortedMap = new HashMap<>();
-        List<String> keys = new LinkedList<>();
+    private static void SortNameAlphabetically(Map<Integer, LapTop> notebookSet) {
 
-        for (Map.Entry<Integer, LapTop> x : Task1.shop.entrySet()) {
-            //System.out.println(x.getValue().GetNameCompany());
-            keys.add(x.getValue().GetMemoryRAM());
-            unsortedMap.put(x.getValue().GetNameCompany(), x.getValue().GetMemoryRAM());
-
+        // code from Oleg.
+        // для того, чтобы проще думать. Запишем все ноутбуки в List, чтобы потом перебрать и сравнить
+        List<LapTop> valueList = new ArrayList<>(notebookSet.values());
+        for (int i = 0; i < valueList.size(); i++) {
+            for (int j = 1; j < valueList.size(); j++) {
+                //System.out.println(valueList.get(i).GetNameCompany()); выводит компанию
+                if (valueList.get(i).GetNameCompany().compareTo(valueList.get(j).GetNameCompany())>1) {
+                    // метод compareTo позволяет сравнивать строки и возвращает положительный результат если первая больше, чем вторая
+                    Collections.swap(valueList, i, j);//меняем местами элементы в коллекции valueList
+                }
+            }
         }
-        //Collections.sort(keys);
+        // перебор и печать
+        for (LapTop lapTop : valueList) {
+            System.out.println(lapTop.getInfo());
+        }
 
     }
 
 
-    public static void SortNameCompany() {
-
-    }
 }
